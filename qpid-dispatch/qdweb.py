a9a4c99cecd248eebfa66e58f319b84d6ac20cc0
Ted Ross <tross@redhat.com>
2014-06-04 14:16:31 -0400

    Cleaned up, added license text

--- qpid-dispatch/qdweb.py
+++ qpid-dispatch/qdweb.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
+##
+## Licensed to the Apache Software Foundation (ASF) under one
+## or more contributor license agreements.  See the NOTICE file
+## distributed with this work for additional information
+## regarding copyright ownership.  The ASF licenses this file
+## to you under the Apache License, Version 2.0 (the
+## "License"); you may not use this file except in compliance
+## with the License.  You may obtain a copy of the License at
+##
+##   http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing,
+## software distributed under the License is distributed on an
+## "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+## KIND, either express or implied.  See the License for the
+## specific language governing permissions and limitations
+## under the License.
+##
+
 import cgi
 import cgitb
 from proton import Messenger, Message, Timeout
@@ -24,7 +43,7 @@ def qdw_title(title):
 
 def qdw_close():
     print '<center>'
-    print 'Powered by <a href="http://qpid.apache.org/components/dispatch-router/index.html">Apache Qpid Dispatch</a> <img src="/index.png" />'
+    print 'Powered by <a href="http://qpid.apache.org/components/dispatch-router/index.html">Apache Qpid Dispatch</a> <img src="/feather.png" />'
     print '</center>'
     print '</body>'
     print '</html>'
--- qpid-dispatch/start.sh
+++ qpid-dispatch/start.sh
@@ -1,14 +1,37 @@
 #!/bin/sh
 
-trap "echo CCCCCCC 2" 2
-trap "echo CCCCCCC 3" 3
-trap "echo CCCCCCC 4" 4
-trap "echo CCCCCCC 5" 5
-trap "echo CCCCCCC 6" 6
-trap "echo CCCCCCC 9" 9
-trap "echo CCCCCCC 15" 15
+##
+## Licensed to the Apache Software Foundation (ASF) under one
+## or more contributor license agreements.  See the NOTICE file
+## distributed with this work for additional information
+## regarding copyright ownership.  The ASF licenses this file
+## to you under the Apache License, Version 2.0 (the
+## "License"); you may not use this file except in compliance
+## with the License.  You may obtain a copy of the License at
+##
+##   http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing,
+## software distributed under the License is distributed on an
+## "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+## KIND, either express or implied.  See the License for the
+## specific language governing permissions and limitations
+## under the License.
+##
+
+##
+## Start the web server
+##
 httpd
+
+##
+## Start the AMQP router
+##
 qdrouterd &
+
+##
+## Tail the web server logs until we're shut down
+##
 tail -f /var/log/httpd/*
 
 
