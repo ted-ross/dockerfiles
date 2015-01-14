561c18de91c85d85f8748244d9f57866c75c726e
Ted Ross <tross@redhat.com>
2015-01-14 15:11:55 -0500

    Added logging, fixed problems with waypoint configuration.

--- dispatch-star/setup.sh
+++ dispatch-star/setup.sh
@@ -22,7 +22,12 @@
 ##
 ## Start the qpid broker
 ##
-qpidd --daemon
+qpidd --daemon -p 11000
+
+##
+## Configure the broker
+##
+qpid-config -b 0.0.0.0:11000 add queue queue/dest.1
 
 ##
 ## Start the routers
